f7461947937368a6e583828144169cb8d442c5ab
shawnbaptista <shawnbaptista@gmail.com>
2018-07-11 22:52:01 -0700

    Practice understanding  and  arrays

---
+++ serverStuff.php
@@ -0,0 +1,24 @@
+<?php
+
+/* 
+ * Requests images and displays them if of a specific file type,
+ * otherwise "Welcome to PHP" will display.
+ * 
+ * Run the file locally with the following command:
+ * php -S localhost:8000 router.php
+ */
+
+echo $_SERVER['SERVER_NAME']; // localhost
+
+if (preg_match('/\.(?:png|jpg|jpeg|gif)$/', $_SERVER["REQUEST_URI"])) {
+    return false; // serve the requested resource as-is.
+} else {
+    echo "<p>Welcome to PHP</p>";
+}
+
+// Print out the PHP $GLOBALS array for viewing pleasure
+echo "<pre>";
+print_r($GLOBALS);
+echo "</pre>";
+
+?>
